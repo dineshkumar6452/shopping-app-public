18c1561efd1b917ebe06fe94caa4e32dcbacbcb6
dinesh-015 <122253632+dinesh-015@users.noreply.github.com>
2025-08-15 18:42:54 +0530

    Todo intergated with Transactions and account

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2506807345"
+    serviceWorkerVersion: "1972703551"
   }
 });
